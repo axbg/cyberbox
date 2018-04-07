4f472ad4e76115c86526506791fa0a917fc2cc2e
Alex Bisag <bisagalexstefan@gmail.com>
2018-04-07 21:15:54 +0300

    v1.0.9 - offline page

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,10 +1,23 @@
 /** An empty service worker! */
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+    // request.mode = navigate isn't supported in all browsers
+    // so include a check for Accept: text/html header.
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+            fetch(event.request.url).catch(error => {
+                // Return the offline page
+                return caches.match("/offline.html");
+            })
+        );
+    }
+    else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
 
 self.addEventListener('install', function(e) {
@@ -13,10 +26,16 @@ self.addEventListener('install', function(e) {
             return cache.addAll([
                 '/',
                 '/index.html',
+                '/offline.html',
                 '/dashboard.html',
                 '/img/blocuri.jpg',
                 '/img/icon.ico',
                 '/img/logo.png',
+                '/css/animate.css',
+                '/css/modal.css',
+                '/css/structure.css',
+                '/css/style.css',
+                '/css/sidenav.css',
             ]);
         })
     );
