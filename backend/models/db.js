4895bd093d13bc4543353ae7c63262e62008d48f
Alex Bișag <bisagalexstefan@gmail.com>
2021-05-12 22:06:49 +0300

    Added dotenv for back-end variables

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,8 @@
 let Sequelize = require('sequelize');
-const sequelize = new Sequelize('db-name', 'db-user', 'db-password', {
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         dialect: 'mysql',
-        host: 'localhost',
+        host: process.env.DB_HOST,
         define: {
            timestamps: false
         }
